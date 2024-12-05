e56bd05be379a8b6f63caf7fcfcb0b3447aaa4c9
MAI <ayoub@hiddenclouders.com>
2024-12-05 22:26:32 +0100

    Add Components

--- src/app/credirect/credirect.module.ts
+++ src/app/credirect/credirect.module.ts
@@ -15,22 +15,58 @@ import { SpeedDialModule } from 'primeng/speeddial';
 import { MessageService } from 'primeng/api';
 import { AddCustomerFileComponent } from './add-customer-file/add-customer-file.component';
 import { StepsModule } from 'primeng/steps';
+import { BankingInformationsComponent } from './banking-informations/banking-informations.component';
+import { BankingDataCommitmentsComponent } from './banking-data-commitments/banking-data-commitments.component';
+import { DataCreditsComponent } from './data-credits/data-credits.component';
+import { BankFileComponent } from './bank-file/bank-file.component';
+import { NotifEmailComponent } from './notif-email/notif-email.component';
+import { InfoCompanyComponent } from './info-company/info-company.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'customer',
     component: ListCustomerFileComponent,
   },
   {
-    path: 'add',
+    path: 'customer/add',
     component: AddCustomerFileComponent,
+  },
+  {
+    path: 'banking-informations',
+    component: BankingInformationsComponent,
+  },
+  {
+    path: 'banking-data-commitments',
+    component: BankingDataCommitmentsComponent,
+  },
+  {
+    path: 'data-credits',
+    component: DataCreditsComponent,
+  },
+  {
+    path: 'bank-file',
+    component: BankFileComponent,
+  },
+  {
+    path: 'notif-email',
+    component: NotifEmailComponent,
+  },
+  {
+    path: 'info-company',
+    component: InfoCompanyComponent,
   },
 ]
 
 @NgModule({
   declarations: [
     ListCustomerFileComponent,
-    AddCustomerFileComponent
+    AddCustomerFileComponent,
+    BankingInformationsComponent,
+    BankingDataCommitmentsComponent,
+    DataCreditsComponent,
+    BankFileComponent,
+    NotifEmailComponent,
+    InfoCompanyComponent
   ],
   imports: [
     CommonModule,
--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -18,18 +18,18 @@ export class AppMenuComponent implements OnInit {
                 label: 'Home',
                 items: [
                     { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] },
-                    { label: 'Fiche client', icon: 'pi pi-fw pi-home', routerLink: ['customer'] },
-                    { label: 'Renseignements Données Bancaires', icon: 'pi pi-fw pi-home'},
-                    { label: 'Engagements Données Bancaires', icon: 'pi pi-fw pi-home'},
-                    { label: 'Données crédit', icon: 'pi pi-fw pi-home'},
-                    { label: 'Dossier Banque', icon: 'pi pi-fw pi-home'}
+                    { label: 'Fiche client', icon: 'pi pi-fw pi-home', routerLink: ['credirect/customer'] },
+                    { label: 'Renseignements Données Bancaires', icon: 'pi pi-fw pi-home' , routerLink: ['credirect/banking-informations']},
+                    { label: 'Engagements Données Bancaires', icon: 'pi pi-fw pi-home', routerLink: ['credirect/banking-data-commitments']},
+                    { label: 'Données crédit', icon: 'pi pi-fw pi-home', routerLink: ['credirect/data-credits']},
+                    { label: 'Dossier Banque', icon: 'pi pi-fw pi-home', routerLink: ['credirect/bank-file']}
                 ]
             },
             {
                 label: 'Autre',
                 items: [
-                    { label: 'Notif email', icon: 'pi pi-fw pi-home'},
-                    { label: 'Infos société', icon: 'pi pi-fw pi-home'},
+                    { label: 'Notif email', icon: 'pi pi-fw pi-home', routerLink: ['credirect/notif-email']},
+                    { label: 'Infos société', icon: 'pi pi-fw pi-home', routerLink: ['credirect/info-company']},
                 ]
             }
         ];
